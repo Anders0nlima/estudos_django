a54424a22f4f71d5f7ff8f22004b7976b87eb533
Anders0nlima <andersoncon85@gmail.com>
2025-09-24 20:52:42 -0300

    criando urls e render html

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def ver_produto(request):
+    return render(request, 'ver_produto.html', {'nome' : 'Anderson'})
+
+def inserir_produtos(request):
+    return HttpResponse("Olá inserir produto")
