5a730692bde0357978be026a9ebff41d56af4f3a
Anders0nlima <andersoncon85@gmail.com>
2025-09-25 10:02:23 -0300

    mais funcionalidades

--- produtos/views.py
+++ produtos/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoas
 
 # Create your views here.
 
 def ver_produto(request):
-    return render(request, 'ver_produto.html', {'nome' : 'Anderson'})
+    if request.method == 'GET':
+       return render(request, 'ver_produto.html', {'nome' : 'Anderson'})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+
+        pessoas = Pessoas(nome = nome, idade = idade)
+        pessoas.save()
+
+        return HttpResponse("Fui chamdado")
+
 
 def inserir_produtos(request):
     return HttpResponse("Olá inserir produto")
